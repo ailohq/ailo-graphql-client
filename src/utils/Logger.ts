b63de972c1bda3ad6c24e7586de6846c97ea6814
Jacek Tomaszewski <jacek@jtom.me>
2020-06-25 15:29:10 +0200

    fix: Make Logger interface align with winston logger

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -5,6 +5,5 @@ export interface Logger {
   error(...data: any[]): void;
   info(...data: any[]): void;
   log(...data: any[]): void;
-  trace(...data: any[]): void;
   warn(...data: any[]): void;
 }
